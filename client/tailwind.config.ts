dd143fc3b2832f6f8fa5cc855055e022b5732f0a
Therese H <therese.fredriksson90@gmail.com>
2025-04-14 15:18:44 +0200

    Add: Styles for headings and other text

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -6,18 +6,19 @@ const config: Config = {
     extend: {
       colors: {
         brand: {
-          black: '#222222',          // Dark black text color
-          blue: '#00A699',           // Brand blue
-          dark: '#333333',           // Dark text color
-          error: '#D9534F',          // Error color (dark red)
-          lightGray: '#F7F7F7',      // Background light gray
-          mediumGray: '#DDDDDD',     // Medium gray
-          shadowGray: '#EBEBEB',     // Light shadow gray
-          red: '#FF5A5F',            // Primary brand red
-          redHoverLight: '#FF385C',  // Hover light red
-          redHoverDark: '#E61E4D',   // Hover dark red
-          success: '#4BB543',        // Success green
-          light: '#FFFFFF',          // Light text color
+          black: '#222222', // Dark black text color
+          blue: '#00A699', // Brand blue
+          dark: '#333333', // Dark text color
+          error: '#D9534F', // Error color (dark red)
+          lightGray: '#F7F7F7', // Background light gray
+          mediumGray: '#DDDDDD', // Medium gray
+          shadowGray: '#EBEBEB', // Light shadow gray
+          red: '#FF5A5F', // Primary brand red
+          redHoverLight: '#FF385C', // Hover light red
+          redHoverDark: '#E61E4D', // Hover dark red
+          success: '#4BB543', // Success green
+          light: '#FFFFFF', // Light text color
+          textLight: '#6a6a6a', // Light text color
         },
       },
     },
