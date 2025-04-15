d24374605dd41e8afde28ba8feb7d9b0220039e2
Alexia Hellsten <alexia.hellsten@gmail.com>
2025-04-15 11:43:03 +0200

    Fix Lucide icon import

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,18 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core'; // Lägg till import
-import { FormsModule } from '@angular/forms'; // Lägg till FormsModule
-
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { routes } from './app.routes';
+import { LucideAngularModule, X, Search } from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-
-    // Lägg till FormsModule här
-    importProvidersFrom(FormsModule),
-  ],
+    ...[
+      importProvidersFrom(LucideAngularModule.pick({ X, Search })),
+      importProvidersFrom(FormsModule)
+    ]
+  ]
 };
