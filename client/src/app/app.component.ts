0a73f24c53fc7f2336f3ada5d0b9e2b023f9643e
Alexia Hellsten <alexia.hellsten@gmail.com>
2025-04-15 18:24:17 +0200

    Add styling to header

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,11 +9,11 @@ import { ButtonComponent } from './components/common/button/button.component';
 import { LinkComponent } from './components/common/link/link.component';
 import { TextInputComponent } from './components/common/form-controls/text-input/text-input.component';
 import { SelectBoxComponent } from './components/common/form-controls/select-box/select-box.component';
-import { CounterOptionComponent } from './components/common/form-controls/select-box/counter-option/counter-option.component'; // Importera CounterOptionComponent här
+import { CounterOptionComponent } from './components/common/form-controls/select-box/counter-option/counter-option.component';
 import { FilterPillComponent } from './components/common/form-controls/filter-pill/filter-pill.component';
 import { TabBarComponent } from './components/common/tab-bar/tab-bar.component';
-import { CheckboxComponent } from './components/common/form-controls/checkbox/checkbox.component'; // Importera CheckboxComponent här
-import { ListingBadgeComponent } from './components/common/listings/listing-badge/listing-badge.component'; // Importera ListingBadgeComponent här
+import { CheckboxComponent } from './components/common/form-controls/checkbox/checkbox.component';
+import { ListingBadgeComponent } from './components/common/listings/listing-badge/listing-badge.component';
 import { FooterComponent } from './components/common/footer/footer.component';
 import { ModalLgComponent } from './components/common/modal-lg/modal-lg.component';
 import { ModalSmComponent } from './components/common/modal-sm/modal-sm.component';
@@ -29,13 +29,13 @@ import { HeaderComponent } from './header/header.component';
     LinkComponent,
     TextInputComponent,
     SelectBoxComponent,
-    CounterOptionComponent, // Lägg till CounterOptionComponent här också
+    CounterOptionComponent,
     FilterPillComponent,
     TabBarComponent,
-    CheckboxComponent, // Lägg till CheckboxComponent här
+    CheckboxComponent,
     ListingBadgeComponent,
     FooterComponent,
-    FormsModule, // Lägg till FormsModule här för ngModel
+    FormsModule,
     ModalLgComponent,
     ModalSmComponent,
     HeaderComponent
--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -3,16 +3,27 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../components/common/button/button.component';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { ModalSmComponent } from '../components/common/modal-sm/modal-sm.component';
+import { LucideAngularModule, Menu, Globe, User } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, ButtonComponent, SearchBarComponent, ModalSmComponent],
+  imports: [
+    CommonModule,
+    ButtonComponent,
+    SearchBarComponent,
+    ModalSmComponent,
+    LucideAngularModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 
 export class HeaderComponent {
+  readonly Menu = Menu;
+  readonly Globe = Globe;
+  readonly User = User;
+
   isMenuOpen = false;
 
   toggleMenu() {
