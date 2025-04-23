dbd3faaa90491c9f96f951d6bc9fd79414b6e161
Therese H <therese.fredriksson90@gmail.com>
2025-04-23 21:10:00 +0200

    Add: New-listing view and listing-type-chapter component style

---
+++ client/src/app/pages/admin-panel/new-listing/chapters/listing-type-chapter/listing-type-chapter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { ToggleCardComponent } from '../../components/toggle-card/toggle-card.component';
+
+@Component({
+  selector: 'app-listing-type-chapter',
+  imports: [ToggleCardComponent],
+  templateUrl: './listing-type-chapter.component.html',
+  styleUrl: './listing-type-chapter.component.css',
+})
+export class ListingTypeChapterComponent {}
--- client/src/app/pages/admin-panel/new-listing/new-listing.component.ts
+++ client/src/app/pages/admin-panel/new-listing/new-listing.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
+import { ListingTypeChapterComponent } from './chapters/listing-type-chapter/listing-type-chapter.component';
+import { WizardFooterComponent } from './components/wizard-footer/wizard-footer.component';
 @Component({
   selector: 'app-new-listing',
-  imports: [],
+  imports: [ListingTypeChapterComponent, WizardFooterComponent],
   templateUrl: './new-listing.component.html',
-  styleUrl: './new-listing.component.css'
+  styleUrl: './new-listing.component.css',
 })
-export class NewListingComponent {
-
-}
+export class NewListingComponent {}
