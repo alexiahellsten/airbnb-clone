09038a72ff37845ec1b4da58e1109ff4168e0961
Alexia Hellsten <alexia.hellsten@gmail.com>
2025-04-27 20:23:09 +0200

    Add price per night chapter styling and HTML structure

--- client/src/app/pages/admin-panel/new-listing/chapters/price-per-night-chapter/price-per-night-chapter.component.ts
+++ client/src/app/pages/admin-panel/new-listing/chapters/price-per-night-chapter/price-per-night-chapter.component.ts
@@ -13,5 +13,17 @@ import { ButtonComponent } from '../../../../../components/common/button/button.
   styleUrl: './price-per-night-chapter.component.css',
 })
 export class PricePerNightChapterComponent {
+  price: number = 0;
 
+  //15% bokningsavgift
+  bookingFeePercentage: number = 15; 
+
+  onPriceChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.price = value ? parseInt(value) : 0;
+  }
+
+  get totalPrice(): number {
+    return this.price + (this.price * (this.bookingFeePercentage / 100));
+  }
 }
