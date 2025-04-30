b8273624a4f53a90ea97d493b142aec0665f6123
Therese H <therese.fredriksson90@gmail.com>
2025-04-30 16:10:34 +0200

    Changed prices to be dynamic

--- client/src/app/components/listing-details/ld-price-overview-card/ld-price-overview-card.component.ts
+++ client/src/app/components/listing-details/ld-price-overview-card/ld-price-overview-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { SelectBoxCounterOptionComponent } from '../../common/form-controls/select-box-counter-option/select-box-counter-option.component';
@@ -18,4 +18,19 @@ import { ButtonComponent } from '../../common/button/button.component';
   templateUrl: './ld-price-overview-card.component.html',
   styleUrl: './ld-price-overview-card.component.css',
 })
-export class LdPriceOverviewCardComponent {}
+export class LdPriceOverviewCardComponent implements OnInit {
+  // Definiera värdena
+  pricePerNight: number = 1400; // Pris per natt
+  nights: number = 7; // Antal nätter
+  cleaningFee: number = 1200; // Städavgift
+  airbnbServiceFee: number = 1944; // Airbnb serviceavgift
+
+  // Beräkna totaler
+  totalPriceForNights: number = this.pricePerNight * this.nights;
+  totalAmount: number =
+    this.totalPriceForNights + this.cleaningFee + this.airbnbServiceFee;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
