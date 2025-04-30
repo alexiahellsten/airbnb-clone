7a2853ad487a3108a6803b97d24f703cd62b4608
Therese H <therese.fredriksson90@gmail.com>
2025-04-30 15:42:53 +0200

    Add option to make button full width

--- client/src/app/components/common/button/button.component.ts
+++ client/src/app/components/common/button/button.component.ts
@@ -18,24 +18,26 @@ export class ButtonComponent {
     | 'pill'
     | 'icon' = 'primary';
   @Input() destination: string = ''; // Input för destination-URL
+  @Input() size: 'fit' | 'full' = 'fit'; // Ny input för storlek
   @Output() buttonClicked = new EventEmitter<void>(); // Output event för klick
 
   get buttonClasses(): string {
+    let widthClass = this.size === 'full' ? 'w-full' : 'w-fit';
     switch (this.buttonType) {
       case 'primary':
-        return 'bg-gradient-to-r from-[#E61E4F] to-[#D80665] text-white rounded-lg font-semibold leading-5 py-3.5 px-6 cursor-pointer';
+        return `${widthClass} bg-gradient-to-r from-[#E61E4F] to-[#D80665] text-white rounded-lg font-semibold leading-5 py-3.5 px-6 cursor-pointer`;
       case 'secondary':
-        return 'text-gray-950 border border-gray-950 rounded-lg font-semibold leading-5 py-3.5 px-6 cursor-pointer';
+        return `${widthClass} text-gray-950 border border-gray-950 rounded-lg font-semibold leading-5 py-3.5 px-6 cursor-pointer`;
       case 'link':
-        return 'text-brand-dark hover:text-black text-lg rounded-lg font-semibold py-3.5 px-6 cursor-pointer';
+        return `${widthClass} text-brand-dark hover:text-black text-lg rounded-lg font-semibold py-3.5 px-6 cursor-pointer`;
       case 'dark':
-        return 'text-white text-lg bg-brand-dark hover:bg-[#000000] rounded-lg font-semibold py-3.5 px-6 cursor-pointer';
+        return `${widthClass} text-white text-lg bg-brand-dark hover:bg-[#000000] rounded-lg font-semibold py-3.5 px-6 cursor-pointer`;
       case 'pill':
-        return 'text-gray-950 border border-gray-400 rounded-full font-semibold leading-5 py-3.5 px-6 cursor-pointer';
+        return `${widthClass} text-gray-950 border border-gray-400 rounded-full font-semibold leading-5 py-3.5 px-6 cursor-pointer`;
       case 'icon':
-        return 'text-white bg-[#FF385C] hover:bg-[#DE1163] rounded-full w-12 h-12 py-0 px-0 text-center font-semibold leading-5 cursor-pointer';
+        return `${widthClass} text-white bg-[#FF385C] hover:bg-[#DE1163] rounded-full w-12 h-12 py-0 px-0 text-center font-semibold leading-5 cursor-pointer`;
       default:
-        return '';
+        return widthClass;
     }
   }
 
