7efa8ab0f00a9e326b75e596aa30bb4864a5cb5a
Therese H <therese.fredriksson90@gmail.com>
2025-05-05 08:15:46 +0200

    Update modal-lg to be able to show content in Booking view

--- client/src/app/pages/booking/booking-cart/booking-cart.component.ts
+++ client/src/app/pages/booking/booking-cart/booking-cart.component.ts
@@ -1,18 +1,22 @@
 import { Component, viewChild } from '@angular/core';
 import { ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { LinkComponent } from '../../../components/common/link/link.component';
 import { ButtonComponent } from '../../../components/common/button/button.component';
 import { ModalLgComponent } from '../../../components/common/modal-lg/modal-lg.component';
 import { CounterOptionComponent } from '../../../components/common/form-controls/select-box-counter-option/counter-option/counter-option.component';
+import { TextInputComponent } from '../../../components/common/form-controls/text-input/text-input.component';
 
 @Component({
   selector: 'app-booking-cart',
   imports: [
+    FormsModule,
     LinkComponent,
     ButtonComponent,
     ModalLgComponent,
     CounterOptionComponent,
+    TextInputComponent,
   ],
   templateUrl: './booking-cart.component.html',
   styleUrl: './booking-cart.component.css',
@@ -21,11 +25,39 @@ export class BookingCartComponent {
   @ViewChild('guestModal') guestModal!: ModalLgComponent;
   @ViewChild('dateModal') dateModal!: ModalLgComponent;
 
+  // Datum
+  checkIn: string = '';
+  checkOut: string = '';
+
+  // Gäster
+  adults: number = 1;
+  children: number = 0;
+  infants: number = 0;
+  pets: number = 0;
+
   openGuestModal() {
     this.guestModal.open();
   }
 
   openDateModal() {
     this.dateModal.open();
+  }
+
+  saveDates() {
+    // Här kan du lägga till logik för att spara datumen
+    this.dateModal.close();
+  }
+
+  cancelDates() {
+    this.dateModal.close();
+  }
+
+  saveGuests() {
+    // Här kan du lägga till logik för att spara gäster
+    this.guestModal.close();
+  }
+
+  cancelGuests() {
+    this.guestModal.close();
   }
 }
