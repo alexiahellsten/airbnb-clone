5c49ce20bac2ccd85714d3affd241b80264080e0
Therese H <therese.fredriksson90@gmail.com>
2025-05-05 07:21:12 +0200

    Fix x-padding to align with other components and added a modal to edit-links in Booking view

--- client/src/app/pages/booking/booking-cart/booking-cart.component.ts
+++ client/src/app/pages/booking/booking-cart/booking-cart.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, viewChild } from '@angular/core';
+import { ViewChild } from '@angular/core';
 
 import { LinkComponent } from '../../../components/common/link/link.component';
 import { ButtonComponent } from '../../../components/common/button/button.component';
+import { ModalLgComponent } from '../../../components/common/modal-lg/modal-lg.component';
+import { CounterOptionComponent } from '../../../components/common/form-controls/select-box-counter-option/counter-option/counter-option.component';
 
 @Component({
   selector: 'app-booking-cart',
-  imports: [LinkComponent, ButtonComponent],
+  imports: [
+    LinkComponent,
+    ButtonComponent,
+    ModalLgComponent,
+    CounterOptionComponent,
+  ],
   templateUrl: './booking-cart.component.html',
   styleUrl: './booking-cart.component.css',
 })
-export class BookingCartComponent {}
+export class BookingCartComponent {
+  @ViewChild('guestModal') guestModal!: ModalLgComponent;
+  @ViewChild('dateModal') dateModal!: ModalLgComponent;
+
+  openGuestModal() {
+    this.guestModal.open();
+  }
+
+  openDateModal() {
+    this.dateModal.open();
+  }
+}
